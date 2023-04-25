b69a77dcc19a2742b85ccbc66c7bdb2e0fa92089
EddyShimwa <shedpray16@gmail.com>
2023-04-25 17:33:47 +0200

    set up the views

--- app/views/recipe_foods/_recipe_food.json.jbuilder
+++ app/views/recipe_foods/_recipe_food.json.jbuilder
@@ -1,4 +1,2 @@
-# frozen_string_literal: true
-
 json.extract! recipe_food, :id, :quantity, :food_id, :recipe_id, :created_at, :updated_at
 json.url recipe_food_url(recipe_food, format: :json)
--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,4 @@ Rails.application.routes.draw do
   resources :recipes
   resources :foods
   resources :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  # Defines the root path route ("/")
-  # root "articles#index"
 end
