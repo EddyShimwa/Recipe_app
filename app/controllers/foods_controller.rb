f8524529d0ff4324975d67728a229f16eb1103ae
peter <peterugwu64@gmail.com>
2023-04-27 09:56:00 +0100

    Update controller and views

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -50,7 +50,7 @@ class FoodsController < ApplicationController
     @food.destroy
 
     respond_to do |format|
-      format.html { redirect_to foods_url, notice: 'Food was successfully destroyed.' }
+      format.html { redirect_to foods_url, notice: 'Food was successfully deleted.' }
       format.json { head :no_content }
     end
   end
