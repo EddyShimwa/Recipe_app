7e438f27e22dc83277e72c0750ca1ddb0a168aa2
peter <peterugwu64@gmail.com>
2023-04-28 15:26:47 +0100

    Add authentications before action and update logics

--- app/controllers/recipe_foods_controller.rb
+++ app/controllers/recipe_foods_controller.rb
@@ -1,4 +1,5 @@
 class RecipeFoodsController < ApplicationController
+  before_action :authenticate_user!
   before_action :set_recipe_food, only: %i[show edit update destroy]
 
   # GET /recipe_foods or /recipe_foods.json
