5b7f81230687adfe76bee6f12a33f09259a150b5
peter <peterugwu64@gmail.com>
2023-04-28 15:38:33 +0100

    Fix linters

--- app/controllers/shopping_list_controller.rb
+++ app/controllers/shopping_list_controller.rb
@@ -10,14 +10,14 @@ class ShoppingListController < ApplicationController
         ingredient.quantity * ingredient.food.price
       end.sum
     end.sum
-    
-      # Reduce quantity to buy if already present
-      @shopping_list.each do |food_name, food|
-        own_food = current_user.foods.find_by(name: food_name)
-        food[:quantity] -= own_food.quantity if own_food
-        food[:value] = food[:quantity] * food[:price]
-      end
-  
-      @food_types = @shopping_list.values.select { |i| (i[:quantity]).positive? }.length
+
+    # Reduce quantity to buy if already present
+    @shopping_list.each do |food_name, food|
+      own_food = current_user.foods.find_by(name: food_name)
+      food[:quantity] -= own_food.quantity if own_food
+      food[:value] = food[:quantity] * food[:price]
+    end
+
+    @food_types = @shopping_list.values.select { |i| (i[:quantity]).positive? }.length
   end
 end
