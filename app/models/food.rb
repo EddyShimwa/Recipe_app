e3564b3687f382d39a1723367e5912fbeca691f6
peter <peterugwu64@gmail.com>
2023-04-28 15:15:25 +0100

    Add methods and update relationships

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,6 +1,7 @@
 class Food < ApplicationRecord
   belongs_to :user
   has_many :recipe_foods, dependent: :destroy
+  has_many :recipes, through: :recipe_foods, source: :recipe
 
   validates :name, presence: true, length: { maximum: 50 }
   validates :price, numericality: { greater_than_or_equal_to: 0 }
